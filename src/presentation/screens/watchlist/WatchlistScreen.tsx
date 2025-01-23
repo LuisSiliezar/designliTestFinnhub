72ceda0fab10043ca8247d97bcb31b6164c56ccf
Luis Siliezar <lsiliezarjr@gmail.com>
2025-01-23 15:52:38 -0600

    feat: added navigation and basic screens

---
+++ src/presentation/screens/watchlist/WatchlistScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+
+const WatchlistScreen = () => {
+
+    return (
+        <View>
+            <Text>Watchlist</Text>
+            <Pressable>
+                <Text>Add</Text>
+            </Pressable>
+        </View>
+    );
+};
+export default WatchlistScreen;
