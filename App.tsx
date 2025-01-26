ca74cd7ebdb10e4c905002473d0ee5387ec04e48
Luis Siliezar <lsiliezarjr@gmail.com>
2025-01-26 11:52:47 -0600

    feat: added notification service for android and ios

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StatusBar, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from '@presentation/routes/StackNavigation';
-import { StatusBar } from 'react-native';
+import NotificationService from '@infrastructure/services/notifications.service';
 
 const App = () => {
+  useEffect(() => {
+    const initializeNotifications = async () => {
+      const permissionGranted = await NotificationService.requestPermission();
+
+      if (permissionGranted) {
+        console.log('Notification permission granted!');
+      } else {
+        console.log('Notification permission denied!');
+      }
+
+      NotificationService.setupListeners();
+
+      await NotificationService.getDeviceToken();
+    };
+    initializeNotifications();
+
+    if (Platform.OS === 'ios') {
+      NotificationService.setBackgroundMessageHandler();
+    }
+  }, []);
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#ffffff" />
